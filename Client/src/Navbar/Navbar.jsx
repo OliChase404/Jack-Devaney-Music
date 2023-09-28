d962bc8644a799f1952d254ce7b4b6de1953db64
Oliver <olichase91@gmail.com>
2023-09-28 14:20:28 -0700

    Navbar

--- Client/src/Navbar/Navbar.jsx
+++ Client/src/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 function Navbar(){
 
+    const [showMenu, setShowMenu] = useState(false)
 
 
     return(
         <div className="Navbar">
-            Navbar
+            <div className="NavbarLeft">
+                <img src="src/assets/homepage Navbar Left.jpg" alt="logo" />
+            </div>
+            <div className="NavbarRight">
+                <img onClick={() => setShowMenu(!showMenu)} src="src/assets/homepage Navbar menu icon.jpg" alt="menu"/>
+            </div>
         </div>
     )
 }
