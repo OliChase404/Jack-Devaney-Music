dd0d98ef497512c663a7004187669db720de280d
Oliver <olichase91@gmail.com>
2023-09-28 18:27:22 -0700

    home page banner with links

--- Client/src/Home/Home.jsx
+++ Client/src/Home/Home.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Home(){
+    const [showAlbumLink, setShowAlbumLink] = useState(false)
 
+    function handleBannerClick(){
+        window.open("https://jackdevaney.bandcamp.com/album/the-phantom-limb")
+    }
 
     return (
         <div className="Home">
-            Home
+
+            <div onClick={() => handleBannerClick()} onMouseEnter={() => setShowAlbumLink(true)} onMouseLeave={() => setShowAlbumLink(false)} className="Banner">
+                {showAlbumLink ? 
+                <img src="src/assets/TPL_ad_bg_Dark.png" alt="Moody Jack" className="MoodyJack"/> : 
+                <img src="src/assets/TPL_ad_bg.png" alt="Moody Jack" className="MoodyJack"/> 
+                }
+                <img src="src/assets/TPL COVERART.png" alt="The Phantom Limb" className="AlbumArt"/>
+                {showAlbumLink ? <div className="AlbumLink">Buy the album on BandCamp!</div> : null}
+            </div>
+
+
+
+
+
         </div>
     )
 }
